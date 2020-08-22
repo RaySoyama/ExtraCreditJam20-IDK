e83c3f5621993908c67bb928eb781874a94f413f
Ray Soyama <raysoyama@gmail.com>
2020-08-21 18:42:23 -0700

    Basic Enemy Controller Template

---
+++ Assets/_ExtraCreditJam20-IDK/Scripts/Enemy/EnemyController.cs
@@ -0,0 +1,85 @@
+﻿using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+    protected string enemyName = "DefaultName";
+    public string EnemyName
+    {
+        get
+        {
+            return enemyName;
+        }
+    }
+
+    protected float enemySpeed = 1;
+    public float EnemySpeed
+    {
+        get
+        {
+            return EnemySpeed;
+        }
+    }
+
+    protected float enemyStartHealth = 1;
+    public float EnemyStartHealth
+    {
+        get
+        {
+            return enemyStartHealth;
+        }
+    }
+
+    protected float enemyHealth = 1;
+    public float EnemyHealth
+    {
+        get
+        {
+            return enemyHealth;
+        }
+    }
+
+    protected float enemyDamage = 1;
+    public float EnemyDamage
+    {
+        get
+        {
+            return enemyDamage;
+        }
+    }
+
+    protected float attackSpeed = 1;
+    public float AttackSpeed
+    {
+        get
+        {
+            return attackSpeed;
+        }
+    }
+
+
+    public delegate void EnemyEvent();
+    public EnemyEvent OnEnemyAttack;
+    public EnemyEvent OnEnemyHit;
+    public EnemyEvent OnEnemyDeath;
+
+    protected virtual void OnSpawn()
+    {
+
+    }
+
+    protected virtual void OnSearch()
+    {
+
+    }
+
+    protected virtual void OnAttack()
+    {
+
+    }
+
+    protected virtual void OnDeath()
+    {
+
+    }
+
+}
