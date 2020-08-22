4c9602b97101135dc1d3c6acb7bd651fffa3e6b3
Ray Soyama <raysoyama@gmail.com>
2020-08-21 22:44:50 -0700

    Updaing Graphics Quality

---
+++ Assets/_ExtraCreditJam20-IDK/Scripts/Pylon/PylonController.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+
+public class PylonController : MonoBehaviour
+{
+    [SerializeField]
+    private float pylonStartHealth = 20;
+    public float PylonStartHealth
+    {
+        get
+        {
+            return pylonStartHealth;
+        }
+    }
+
+    private float pylonCurrentHealth = 20;
+    public float PylonCurrentHealth
+    {
+        get
+        {
+            return pylonCurrentHealth;
+        }
+    }
+
+    private void Start()
+    {
+        pylonCurrentHealth = PylonStartHealth;
+    }
+
+    public void TakeDamage(float damage)
+    {
+        //damage number validation
+
+        pylonCurrentHealth -= damage;
+
+        if (PylonCurrentHealth <= 0)
+        {
+            //dead
+        }
+    }
+}
