7c4dad12c4b771bcd804870a60619c06a49e34d1
Alexzoin <alex.cecrle@gmail.com>
2020-08-23 18:59:13 -0500

    Turtle dialogue

---
+++ Assets/_ExtraCreditJam20-IDK/Scripts/TurtleController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurtleController : MonoBehaviour
+{
+	private AudioSource audio;
+
+	public AudioClip introduction;
+	private bool hasPlayedIntro = false;
+
+
+    void Start()
+    {
+		audio = GetComponent<AudioSource>();
+    }
+
+    
+    void Update()
+    {
+		if (Time.time > 1f && !hasPlayedIntro)
+		{
+			audio.PlayOneShot(introduction);
+			hasPlayedIntro = true;
+		}
+    }
+}
