1c028ed3a29ba980a6c4cbeb6345737be57f2af8
Camzoin <Camden.Cecrle@gmail.com>
2020-08-23 22:50:02 -0700

    Main Menu in place

---
+++ Assets/_ExtraCreditJam20-IDK/Scripts/SceneFunctions.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class SceneFunctions : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void CloseGame()
+    {
+        Application.Quit();
+    }
+
+    public void ChangeScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+}
