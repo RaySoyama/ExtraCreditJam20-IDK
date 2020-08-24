0b4c9de0bbd2986cdd76c40bbf2b571e24cb4e50
Ray Soyama <raysoyama@gmail.com>
2020-08-24 10:44:41 -0700

    Tutorial system working

---
+++ Assets/_ExtraCreditJam20-IDK/Scripts/UI/TutorialManager.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class TutorialManager : MonoBehaviour
+{
+    [SerializeField]
+    private List<GameObject> AllSlides = new List<GameObject>();
+
+    [SerializeField, ReadOnlyField]
+    private int currentSlide = 0;
+
+    [SerializeField]
+    private SceneFunctions sceneFunc = null;
+
+    [SerializeField, ReadOnlyField]
+    private string targetScene = "exit";
+
+    void Start()
+    {
+        currentSlide = 0;
+
+        DisabelAllSlides();
+    }
+
+    [ContextMenu("Start Slides")]
+    public void StartSlides()
+    {
+        AllSlides[currentSlide].SetActive(true);
+    }
+
+    public void StartSlides(string sceneName)
+    {
+        AllSlides[currentSlide].SetActive(true);
+        targetScene = sceneName;
+    }
+
+    [ContextMenu("Next Slide")]
+    public void NextSlide()
+    {
+        if (currentSlide == AllSlides.Count - 1)
+        {
+            DisabelAllSlides();
+            PostTutorialAction();
+            return;
+        }
+        AllSlides[currentSlide].SetActive(false);
+        currentSlide++;
+        AllSlides[currentSlide].SetActive(true);
+    }
+
+    public void DisabelAllSlides()
+    {
+        foreach (GameObject slide in AllSlides)
+        {
+            slide.SetActive(false);
+        }
+    }
+
+    public void PostTutorialAction()
+    {
+        if (targetScene == "exit")
+        {
+            sceneFunc.CloseGame();
+            return;
+        }
+
+        sceneFunc.ChangeScene(targetScene);
+    }
+
+}
