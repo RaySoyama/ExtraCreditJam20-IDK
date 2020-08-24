88d4b202de514bc4f73aec1881c9f353924d15da
Camzoin <Camden.Cecrle@gmail.com>
2020-08-23 23:13:27 -0700

    added pause

---
+++ Assets/_ExtraCreditJam20-IDK/Scripts/Pause.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pause : MonoBehaviour
+{
+    public GameObject pauseMenu;
+
+    bool isPaused = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown("escape"))
+        {
+            isPaused = !isPaused;
+        }
+
+        if(isPaused)
+        {
+            //pause
+
+            pauseMenu.SetActive(true);
+
+
+            Time.timeScale = 0;
+
+            Cursor.visible = true;
+            Cursor.lockState = CursorLockMode.Confined;
+        }
+        else
+        {
+            //unpause
+
+            pauseMenu.SetActive(false);
+
+            Time.timeScale = 1.0f;
+
+            Cursor.visible = false;
+            Cursor.lockState = CursorLockMode.Locked;
+        }
+    }
+
+    public void Unpause()
+    {
+        isPaused = false;
+    }
+}
